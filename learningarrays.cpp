b6c627c080c708b0ad0d668d06847e0548a75924
fritzchentastig <fritzchen2005@gmail.com>
2023-01-18 11:34:35 +0100

    more arrays

--- learningarrays.cpp
+++ learningarrays.cpp
@@ -8,7 +8,16 @@ using namespace std;
 void arraytest();
 //Arrays (Vektoren, Felder)
  //Used to store a lot of data of the same Type
- int num[3]={2,4,6}
- int numone[]={2,4,6}
+ int num[3]={2,4,6};
+ int numone[]={2,4,6};
  //numtwo[0-2] gets these Data the rest is written out with zero
- int numtwo[10]={2,4,6}
+ int numtwo[10]={2,4,6};
+
+ float firstarray[100];
+ firstarray[17]=9;
+ firstarray[0]=3.4;
+ firstarray[99]=-15.5;
+ int secarray[5];
+ int thirarray[]={5,4,3,2,1}
+ char fouarray[10];
+ fourarry[0]="L";
